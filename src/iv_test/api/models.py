1bc5d448a250c5c594cb3f9cbc24f7f55e0cbc42
Shota Koide <s.koide@qufooit.com>
2024-01-01 18:08:13 +0900

    model def

---
+++ src/iv_test/api/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+
+class AiAnalysisLog(models.Model):
+    image_path = models.CharField(max_length=255)
+    success = models.CharField(max_length=255)
+    message = models.CharField(max_length=255)
+    class_id = models.IntegerField(db_column='class')
+    confidence = models.DecimalField(max_digits=5, decimal_places=4)
+    request_timestamp = models.IntegerField()
+    response_timestamp = models.IntegerField()
+
+    class Meta:
+        db_table = 'ai_analysis_log'
